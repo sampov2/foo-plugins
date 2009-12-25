dd82da1d240ab1d35141d6ff607ce75cae8e924b
sampo.savolainen <sampo.savolainen@4f5fdede-0da1-11de-97d8-e990b0f60ea0>
2009-12-25 23:54:17 +0000

    New version of the compressor, now with measurement outputs. Measuring code is in C++ in the LV2 "adaptor". Dirty in one way, but allows for integration with unmodified faust generated code.

--- src/schmooz_mono.cpp
+++ src/schmooz_mono.cpp
@@ -20,6 +20,8 @@
 #include <cmath>
 #include "utils.h"
 
+//#include <iostream>
+
 inline float max(float a, float b) { return fmax(a, b); }
 inline float min(float a, float b) { return fmin(a, b); }
 
@@ -44,12 +46,16 @@ using namespace std;
 
 class SchmoozMono : public Plugin<SchmoozMono>, UI {
 public:
-    SchmoozMono(double rate)
-          : Plugin<SchmoozMono>(10)
+  SchmoozMono(double rate)
+        : Plugin<SchmoozMono>(10)
+	, max_nframes(32768)
   {
 	schmooz_mono.instanceInit( (int)rate );
 
 	schmooz_mono.buildUserInterface(this);
+
+	input_measurement_buffer       = new float[max_nframes];
+	attenuation_measurement_buffer = new float[max_nframes];
   }
 
   void run(uint32_t nframes) 
@@ -62,11 +68,63 @@ public:
 	*makeup_gain_db 	= *p(PORT_MAKEUP);
 	*dry_wet_balance 	= *p(PORT_DRYWET);
 
-	float gain;
+	uint32_t n = 0;
+	float *outputBuffers[3];
 
-	schmooz_mono.compute(nframes, &p(PORT_AUDIO_INPUT), &p(PORT_AUDIO_OUTPUT), &gain);
+	float input_peak = -70.0;
+	float compressed_peak = 200;
+	float full_atten_gain = 0;
+	float full_atten_gain_abs = 0;
 
-	*p(PORT_OUTPUT_ATTENUATION) = CO_DB(gain);
+	while ( n < nframes ) {
+
+		outputBuffers[0] = p(PORT_AUDIO_OUTPUT) + n;
+		outputBuffers[1] = attenuation_measurement_buffer + n;
+		outputBuffers[2] = input_measurement_buffer + n;
+
+		uint32_t i = nframes - n;
+		if (i > max_nframes) {
+			i = max_nframes;
+		}
+		//std::cerr << " i = " << i << std::endl;
+
+		schmooz_mono.compute(i, &p(PORT_AUDIO_INPUT) + n, outputBuffers);
+
+		n += i;
+
+		while ( i > 0) {
+			--i;
+			// current input (after HPF and RMS follower)
+			float curr_input = CO_DB(input_measurement_buffer[i]);
+
+			float compgain   = attenuation_measurement_buffer[i]; // in dB
+
+			// current raw compressed input is input adjusted 
+			// by compressor gain with makeup negated
+			float curr_comp  = curr_input + compgain - *makeup_gain_db;
+
+			// full attenuation (gain) is compgain adjusted by dry/wet
+			float full = compgain * *dry_wet_balance;
+			float full_abs = fabsf(full);
+
+			// is max right, or should we use some sort of smoothing?
+			input_peak       = fmaxf(input_peak, curr_input);
+			compressed_peak  = fmaxf(compressed_peak, curr_comp);
+
+			// 
+			if (full_atten_gain_abs < full_abs) {
+				full_atten_gain = full;
+				full_atten_gain_abs = full_abs;
+			}
+		}
+
+	}
+	//std::cerr << "--------" << std::endl;
+	//std::cerr << "input_peak = " << input_peak << std::endl;
+	//std::cerr << "compressed_peak = " << compressed_peak << std::endl;
+	//std::cerr << "full_atten_gain = " << full_atten_gain << std::endl;
+
+	*p(PORT_OUTPUT_ATTENUATION) = full_atten_gain;
   }
 
   // Unused UI-functions        
@@ -125,6 +183,9 @@ public:
 
 	mydsp schmooz_mono;
 
+	uint32_t max_nframes;
+	float   *input_measurement_buffer;
+	float   *attenuation_measurement_buffer;
 };
 
 static int _ = SchmoozMono::register_class("http://studionumbersix.com/foo/lv2/schmooz-mono");
