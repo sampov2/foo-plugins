4e8bc47a141fcb7e2f178562f1c0a157865f83ac
sampo.savolainen <sampo.savolainen@4f5fdede-0da1-11de-97d8-e990b0f60ea0>
2010-01-31 14:23:10 +0000

    Add a few different polyblep functions. The original is still the best..

--- incubator/foo-yc20/polyblep.cpp
+++ incubator/foo-yc20/polyblep.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 
-inline float calculate_polyblep(float t)
+inline float calculate_polyblep_valimaki_huovilainen(float t)
 {
 	if (t>0) {
 		return (t - (t*t)/2.0 - 0.5);
@@ -11,7 +11,39 @@ inline float calculate_polyblep(float t)
 	}
 }
 
-#define polyblep_it(x) calculate_polyblep((x))
+// These have slightly less high frequency aliasing bands but have much more sub-harmonic aliasing
+// than the one by Välimäki & Huovilainen.
+inline float calculate_polyblep_pekonen(float t)
+{
+	if (t>0) {
+		return t*t*t*t*3/14 - t*t*t*4/7 + t*6/7 - 0.5;
+	} else {
+		return -t*t*t*t*3/14 - t*t*t*4/7 + t*6/7 + 0.5;
+	}
+
+}
+
+inline float calculate_polyblep_pekonen2(float t)
+{
+	if (t>0) {
+		return t*t*t*t*3/26 - t*t*t*4/13 - t*t*3/13 + t*24/26 - 0.5;
+	} else {
+		return -t*t*t*t*3/26 - t*t*t*4/13 + t*t*3/13 + t*24/26 + 0.5;
+	}
+
+}
+
+// my attempt
+inline float calculate_polyblep_savolainen(float t)
+{
+	return  t * (1.0-fabsf(t) * 0.5) + 0.5 - 1*(t>0);
+}
+
+//#define polyblep_it(x) calculate_polyblep_savolainen((x))
+#define polyblep_it(x) calculate_polyblep_valimaki_huovilainen((x))
+
+//#define polyblep_it(x) calculate_polyblep_pekonen((x))
+//#define polyblep_it(x) calculate_polyblep_pekonen2((x))
 
 float square_blep_x(int select, float x, float ph, float q)
 {
@@ -47,7 +79,7 @@ float saw_blep_x(int select, float x, float ph, float q)
 		return x;
 	case 1:
 	default:
-		return x + polyblep_it(ph / q)*2;
+		return x + polyblep_it(ph / q) * 2.0;
 	}
 }
 
@@ -58,6 +90,6 @@ float saw_blep_prev(int select, float prev, float ph_1, float q)
 		return prev;
 	case 1:
 	default:
-		return prev + polyblep_it((ph_1 - 1.0)/q) * 2;
+		return prev + polyblep_it((ph_1 - 1.0)/q) * 2.0;
 	}
 }
