394282c6e737590e1fdf61a2e86fa165abbc66bf
sampo.savolainen <sampo.savolainen@4f5fdede-0da1-11de-97d8-e990b0f60ea0>
2010-04-05 21:06:18 +0000

    Working, but the balance should be switched around

--- trunk/incubator/foo-yc20/foo-yc20.cpp
+++ trunk/incubator/foo-yc20/foo-yc20.cpp
@@ -1,10 +1,33 @@
+/*
+    Foo-YC20 UI
+    Copyright (C) 2009  Sampo Savolainen <v2@iki.fi>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #include <iostream>
+
+#include <jack/jack.h>
+#include <jack/midiport.h>
 
 #include <gtkmm/main.h>
 #include <gtkmm/window.h>
 #include <gtkmm/drawingarea.h>
 
+#include <cstring>
+#include <cerrno>
 #include <list>
 #include <set>
 
@@ -38,6 +61,13 @@ namespace Wdgt {
 		load_png("green_3.png") };
 };
 
+mydsp         *yc20 = NULL;
+
+jack_port_t   *audio_output_port = NULL;
+jack_port_t   *midi_input_port = NULL;
+jack_client_t *jack_client = NULL;
+
+float         *yc20_keys[61];
 
 
 class YC20UI :  public UI
@@ -50,7 +80,7 @@ class YC20UI :  public UI
 		Gtk::Widget *getWidget() { return &_drawingArea; }
 
 		// from Faust UI
-		void addButton(const char* label, float* zone) {};
+		void addButton(const char* label, float* zone);
 		void addToggleButton(const char* label, float* zone) {};
 		void addCheckButton(const char* label, float* zone) {};
 		void addVerticalSlider(const char* label, float* zone, float init, float min, float max, float step);
@@ -246,6 +276,46 @@ YC20UI::YC20UI()
 	percussion->setName("percussion");
 
 	wdgts.push_back(percussion);
+}
+
+void
+YC20UI::addButton(const char* label, float* zone)
+{
+	bool isNote = true;
+	int note;
+	int octave = atoi(label+1);
+
+	if (strlen(label) != 2) {
+		isNote = false;
+	}
+
+	if (octave == 0 && errno == EINVAL) {
+		isNote = false;
+	}
+
+	switch(label[0]) {
+		case 'c': note =  0; break;
+		case 'C': note =  1; break;
+		case 'd': note =  2; break;
+		case 'D': note =  3; break;
+		case 'e': note =  4; break;
+		case 'f': note =  5; break;
+		case 'F': note =  6; break;
+		case 'g': note =  7; break;
+		case 'G': note =  8; break;
+		case 'a': note =  9; break;
+		case 'A': note = 10; break;
+		case 'b': note = 11; break;
+		default: isNote = false;
+	}
+
+	if (isNote) {
+		//std::cerr << "Connected key " << label << ", octave " << octave << ", note " << note << std::endl;
+		//fprintf(stderr, "Connected key %s, octave %d, note %d\n", label, octave, note);
+		yc20_keys[octave*12 + note] = zone;
+		return;
+	}
+
 }
 
 void
@@ -532,39 +602,158 @@ YC20UI::expose(GdkEventExpose *evt)
 	return true;
 }
 
+int
+process (jack_nframes_t nframes, void *arg)
+{
+	float * output_buffer = (float *)jack_port_get_buffer(audio_output_port, nframes);
+
+        void *midi = jack_port_get_buffer(midi_input_port, nframes);
+        jack_midi_event_t event;
+        jack_nframes_t n = jack_midi_get_event_count(midi);
+
+        if (n > 0) {
+
+            // TODO: frame accuracy
+            // TODO: panic button
+            for (uint32_t i = 0; i < n; ++i) {
+                jack_midi_event_get(&event, midi, i);
+
+                int note = -1;
+                float value = 0.0;
+
+                if( ((*(event.buffer) & 0xf0)) == 0x90 )
+                {
+                        /* note on */
+                        note = *(event.buffer + 1) - 36;
+                        value = 1.0;
+                }
+                else if( ((*(event.buffer)) & 0xf0) == 0x80 )
+                {
+                        /* note off */
+                        note = *(event.buffer + 1) - 36;
+                        value = 0.0;
+                } else if ( ((*(event.buffer)) & 0xf0) ==  0xb0) {
+                        int cc    = *(event.buffer+1);
+                        int value = *(event.buffer+2);
+
+                        // while one shouldn't lock inside an RT thread, the 
+                        // other party aquiring this lock is always O(1) in the
+                        // locked state
+
+			/* TODO: CC's, CC's, CC's
+                        sem_wait(&controlChangeQueueSem);
+                        controlChangeQueue.push_back( new MidiCC(cc, value));
+                        sem_post(&controlChangeQueueSem);
+			*/
+                }
+
+                if (note >= 0 && note < 61) {
+                        *yc20_keys[note] = value;
+                }
+
+            }
+
+        }
+
+        yc20->compute(nframes, NULL, &output_buffer);
+
+
+	return 0;
+}
+
+
+
+void disconnect_from_jack(void *arg)
+{
+	std::cerr << "Disconnected from jack.. bummer" << std::endl;
+	jack_client = NULL;
+	midi_input_port = NULL;
+	audio_output_port = NULL;
+}
+
+
+bool connect_to_jack()
+{
+        jack_options_t options = JackNullOption;
+        jack_status_t status;
+
+        jack_client = jack_client_open ("Foo YC20", options, &status, NULL);
+	if (jack_client == NULL) {
+		std::cerr << "jack_client_open() failed" << std::endl;
+
+		midi_input_port = NULL;
+		audio_output_port = NULL;
+		return false;
+	}
+
+
+        midi_input_port = jack_port_register (jack_client, "midi in",
+                                              JACK_DEFAULT_MIDI_TYPE,
+                                              JackPortIsInput, 0);
+        audio_output_port = jack_port_register (jack_client, "output",
+                                                JACK_DEFAULT_AUDIO_TYPE,
+                                                JackPortIsOutput, 0);
+
+        jack_set_process_callback (jack_client, process, 0);
+
+        /* tell the JACK server to call `jack_shutdown()' if
+           it ever shuts down, either entirely, or if it
+           just decides to stop calling us.
+        */
+
+        jack_on_shutdown (jack_client, disconnect_from_jack, 0);
+
+
+        if (jack_activate (jack_client)) {
+                std::cerr << "cannot activate client" << std::endl;
+		jack_client_close(jack_client);
+		jack_client = NULL;
+		midi_input_port = NULL;
+		audio_output_port = NULL;
+		return false;
+        }
+
+	return true;
+}
+
 
 
 int main(int argc, char **argv)
 {
+
         Gtk::Main mymain(argc, argv);
 
-	mydsp       yc20;
 
 	Gtk::Window *main_window;
 	YC20UI      *yc20ui;
 
 
 	main_window = new Gtk::Window();
+	main_window->set_title("Foo YC20");
+
 	yc20ui = new YC20UI();
 
-	yc20.init(48000);
-	yc20.buildUserInterface(yc20ui);
-
-	main_window->set_title("Foo YC20");
 
 
 	main_window->add(*yc20ui->getWidget());
 
-	//main_window->show_all();
 	main_window->show();
 	yc20ui->getWidget()->show();
+
+
+	if (!connect_to_jack()) {
+		return 1;
+	}
+
+	yc20 = new mydsp();
+	yc20->init(jack_get_sample_rate (jack_client));
+	yc20->buildUserInterface(yc20ui);
 
         Gtk::Main::run(*main_window);
 
 	delete main_window;
-
 	delete yc20ui;
-
+	delete yc20;
 
 	return 0;
 }
